37747423a64fcbd7972955ca159b5ce20ce154f1
Nathan <nathanbrentlloyd2000@gmail.com>
2026-02-17 10:13:13 +0000

    Fixed import issue, removed queue and knight travails code and replaced with imports

--- graph/src/ui.js
+++ graph/src/ui.js
@@ -1,4 +1,6 @@
-// I'll fix the webpack/import issue later
+
+import { knightMoves, getAdjacent } from "../graph.js";
+
 class UI {
     static #body = () => document.querySelector('body');
     static #container = () => document.querySelector('.container');
@@ -31,9 +33,8 @@ class UI {
         let id;
         let endId;
         let startSet = false;
-        let cpy = Array.from(document.querySelectorAll('.grid'));
         Array.from(document.querySelectorAll('.grid')).forEach((square) => {
-            square.addEventListener('click', (e) => {
+            square.addEventListener('click', () => {
                 if (!startSet) {
                     id = square.id;
                     startSet = true;
@@ -75,14 +76,14 @@ class UI {
         document.querySelector('.how-to').textContent = `Watch him go!`;
         for (const pos of start) {
             this.highlightEndPos(end);
-            this.makeMove(pos, start);
+            await this.makeMove(pos, start);
             this.#endHeader().textContent = `Current: s${pos[0]}-${pos[1]}`;
             this.#moveCounter().textContent = `Moves: ${moves++}`;
 
             if (pos !== start[start.length - 1])
                 this.highlightPossibleMoves(pos);
 
-            await this.sleep(1500);
+            await this.sleep(500);
 
             if (pos !== start[start.length - 1]) {
                 document.querySelector(`#s${pos[0]}-${pos[1]}`).innerHTML = ``;
@@ -136,86 +137,6 @@ class UI {
 
     static animateMove() {
 
-    }
-}
-
-function knightMoves(start, end) {
-    const visited = new Set();
-    const parent = new Set();
-    const queue = new Queue();
-    queue.enqueue(start);
-    parent[`${start[0]}, ${start[1]}`] = null;
-
-    while (!queue.isEmpty()) {
-        let vertex = queue.front();
-        let adjacent = getAdjacent(vertex);
-        let key = `${vertex[0]}, ${vertex[1]}`;
-        visited[key] = key;
-        for (const v of adjacent) {
-            if (!visited[`${v[0]}, ${v[1]}`]) {
-                visited[`${v[0]}, ${v[1]}`] = v;
-                parent[`${v[0]}, ${v[1]}`] = `${vertex[0]}, ${vertex[1]}`;
-                queue.enqueue(v);
-            }
-        }
-
-        let shortestPath = [];
-        if (vertex[0] === end[0] && vertex[1] === end[1]) {
-            let pnt = parent[`${vertex[0]}, ${vertex[1]}`];
-            shortestPath.unshift(vertex);
-
-            while (pnt !== null) {
-                if (pnt) {
-                    let [x, y] = pnt.split(', ');
-                    shortestPath.unshift([+x, +y]);
-                }
-                pnt = parent[pnt];
-            }
-            let moves = shortestPath.length - 1;
-            return shortestPath;
-        }
-        queue.dequeue();
-    }
-}
-
-function getDelta(pos) {
-    return [
-        [1, 2], [-1, 2], [1, -2], [-1, -2],
-        [2, 1], [-2, 1], [2, -1], [-2, -1]
-    ].map((i) => i)
-    .filter((i) =>
-        (pos[0] + i[0] >= 0 && pos[0] + i[0] < 8) &&
-        (pos[1] + i[1] >= 0 && pos[1] + i[1] < 8)
-    );
-}
-
-function getAdjacent(pos) {
-    return getDelta(pos).map((e) => [pos[0] + e[0], pos[1] + e[1]]);
-}
-
-class Queue {
-    constructor() {
-        this.queue = [];
-    }
-
-    dequeue() {
-        if (this.isEmpty()) return;
-        this.queue.shift();
-    }
-
-    isEmpty = () => !this.queue.length;
-    getQueue = () => this.queue;
-    size = () => this.queue.length;
-    enqueue = (val) => this.queue.push(val);
-
-    front() {
-        if (this.isEmpty()) return;
-        return this.queue[0];
-    }
-
-    last() {
-        if (this.isEmpty()) return;
-        return this.queue[this.queue.length - 1];
     }
 }
 
