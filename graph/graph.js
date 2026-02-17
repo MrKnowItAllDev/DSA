7f35e190b3451120734d36fd84856a07bbf9a5da
Nathan <nathanbrentlloyd2000@gmail.com>
2026-02-17 09:51:13 +0000

    Code refactor

--- graph/graph.js
+++ graph/graph.js
@@ -6,41 +6,36 @@ export function knightMoves(start, end) {
     const visited = new Set();
     const parent = new Set();
     const queue = new Queue();
+
     queue.enqueue(start);
-    parent[`${start[0]}, ${start[1]}`] = null;
+    parent[start.toString()] = null;
 
     while (!queue.isEmpty()) {
         let vertex = queue.front();
-        let adjacent = getAdjacent(vertex);
-        let key = `${vertex[0]}, ${vertex[1]}`;
-        visited[key] = key;
-        for (const v of adjacent) {
-             if (!visited[`${v[0]}, ${v[1]}`]) {
-                 visited[`${v[0]}, ${v[1]}`] = v;
-                 parent[`${v[0]}, ${v[1]}`] = `${vertex[0]}, ${vertex[1]}`;
-                 queue.enqueue(v);
-             }
-        }
 
         let shortestPath = [];
         if (vertex[0] === end[0] && vertex[1] === end[1]) {
-            let pnt = parent[`${vertex[0]}, ${vertex[1]}`];
+            let pnt = parent[vertex.toString()];
             shortestPath.unshift(vertex);
-
             while (pnt !== null) {
                 if (pnt) {
-                    let [x, y] = pnt.split(', ');
+                    let [x, y] = pnt.split(',');
                     shortestPath.unshift([+x, +y]);
                 }
                 pnt = parent[pnt];
             }
-            let moves = shortestPath.length - 1;
-            console.log(`You made it in ${moves} ${ moves > 1 ? 'moves': 'move' }! Here's your path:`);
-
-            shortestPath.forEach((path) => {
-                console.log(path);
-            });
             return shortestPath;
+        }
+
+        let adjacent = getAdjacent(vertex);
+        let key = vertex.toString();
+        visited[key] = key;
+        for (const v of adjacent) {
+            if (!visited[v.toString()]) {
+                visited[v.toString()] = v;
+                parent[v.toString()] = vertex.toString();
+                queue.enqueue(v);
+            }
         }
         queue.dequeue();
     }
@@ -61,4 +56,13 @@ function getAdjacent(pos) {
     return getDelta(pos).map((e) => [pos[0] + e[0], pos[1] + e[1]]);
 }
 
-knightMoves([0,0],[2,1]);
+function printPath(start, end) {
+    let path = knightMoves(start, end);
+    let moves = path.length - 1;
+    path.forEach((path, i) => {
+        if (i === 0) console.log(`You made it in ${moves} ${moves !== 1 ? 'moves' : 'move'}! Here's your path:`);
+        console.log(path);
+    });
+}
+
+printPath([0, 0], [4, 6]);
